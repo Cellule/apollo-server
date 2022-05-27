95c05b5984c07c39fe2c8402eace4ff00cdbd48f
Trevor Scheer <trevor.scheer@gmail.com>
2022-05-27 15:53:47 -0700

    Simplify standalone server API

--- packages/server/src/__tests__/standalone/integration.test.ts
+++ packages/server/src/__tests__/standalone/integration.test.ts
@@ -1,5 +1,3 @@
-import type http from 'http';
-import type { AddressInfo } from 'net';
 import { ApolloServer } from '../../ApolloServer';
 import type { BaseContext } from '../../externalTypes';
 import { standaloneServer } from '../../standalone';
@@ -16,24 +14,10 @@ defineIntegrationTestSuite(async function (
 ): Promise<CreateServerForIntegrationTestsResult> {
   const opts = testOptions ? { context: testOptions.context } : undefined;
   const server = new ApolloServer(serverOptions);
-  const standaloneServerInstance = standaloneServer(server, opts);
-  await standaloneServerInstance.listen({ port: 0 });
+  const { url } = await standaloneServer(server, opts, { port: 0 });
 
   return {
     server,
-    url: urlForHttpServer(standaloneServerInstance['httpServer']),
+    url,
   };
 });
-
-function urlForHttpServer(httpServer: http.Server): string {
-  const { address, port } = httpServer.address() as AddressInfo;
-
-  // Convert IPs which mean "any address" (IPv4 or IPv6) into localhost
-  // corresponding loopback ip. Note that the url field we're setting is
-  // primarily for consumption by our test suite. If this heuristic is wrong for
-  // your use case, explicitly specify a frontend host (in the `host` option to
-  // ApolloServer.listen).
-  const hostname = address === '' || address === '::' ? 'localhost' : address;
-
-  return `http://${hostname}:${port}`;
-}
